ea565c8bf2bcfa43619dd4f4390418d759bc3bc7
Gabriel Mochi <gmochi56@icloud.com>
2020-12-11 01:02:35 -0300

    implements basic structure

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+
+// TODO: convert modules to typescript
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
